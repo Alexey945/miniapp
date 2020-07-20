568474ede46cde847cd5a3430a11cbd3be2056e3
Алексей <Alekseyshaposnikov7@gmail.com>
2020-07-20 16:46:46 +0300

    Updates

---
+++ precache-manifest.b9155dc6d3e93c286136baae7d8bf056.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cae0afebb9abeef038a7f253be41e6fc",
+    "url": "/miniapp/index.html"
+  },
+  {
+    "revision": "a6d3e93818d6c4661ba0",
+    "url": "/miniapp/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "a6d3e93818d6c4661ba0",
+    "url": "/miniapp/static/js/2.1375672c.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/miniapp/static/js/2.1375672c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04a278551c730ebc0d48",
+    "url": "/miniapp/static/js/main.cb52efb1.chunk.js"
+  },
+  {
+    "revision": "0e680c1efb9f2803105b",
+    "url": "/miniapp/static/js/runtime-main.f089e66c.js"
+  }
+]);
