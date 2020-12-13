9bbd185204fde8ac5ae2f6034a302d8495f73624
Aleksei <alekseyshaposnikov7@gmail.com>
2020-12-13 18:06:01 +0300

    Updates

--- precache-manifest.c7d2c40a6566f0776b7c6253cccef949.js
+++ precache-manifest.c7d2c40a6566f0776b7c6253cccef949.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9dbcfabf312d8b1f38c3046ae3ea63b1",
+    "revision": "ad55605158babcaaa4260d47790aaf4c",
     "url": "/miniapp/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/miniapp/static/css/2.ed87aa75.chunk.css"
   },
   {
-    "revision": "aaa7e180831e6c42475f",
+    "revision": "b8aba3b52f804509a42d",
     "url": "/miniapp/static/css/main.53f650fe.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/miniapp/static/js/2.d7dbfcae.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "aaa7e180831e6c42475f",
-    "url": "/miniapp/static/js/main.b984ac47.chunk.js"
+    "revision": "b8aba3b52f804509a42d",
+    "url": "/miniapp/static/js/main.3a5ff161.chunk.js"
   },
   {
     "revision": "0e680c1efb9f2803105b",
